ff69394438aa6752b119973f2f58e2b752a54ab4
Hajinsol <jinsor5542@gmail.com>
2014-12-11 14:54:07 +0900

    2

---
+++ sketch.js
@@ -0,0 +1,71 @@
+ createCanvas(500, 500);
+}
+
+function draw() {
+   background(bg);
+  
+   fill(255,64,64);
+   noStroke()
+   image(img6, 140,140,270,270);
+   
+   var duration = 4000;
+   var timing = (new Date()%duration)/duration;
+
+   fill(229,211,136);
+   noStroke()
+   image(img4,250 + Math.cos(timing*2*PI)*200,           
+        250 + Math.sin(timing*2*PI)*200,       
+        40,                                     
+        40);                                    
+   
+   var duration = 5000;
+   var timing = (new Date()%duration)/duration;
+
+   fill(87,96,255);
+   noStroke()
+   image(img3, 250 + Math.cos(timing*2*PI)*350,           
+        250 + Math.sin(timing*2*PI)*200,       
+        150,                                     
+        150);                                    
+   
+   var duration = 8000;
+   var timing = (new Date()%duration)/duration;
+
+   fill(126,22,12);
+   noStroke()
+   image(img5, 250 + Math.cos(timing*2*PI)*250,           
+        250 + Math.sin(timing*2*PI)*200,       
+        60,                                     
+        60);                                    
+   
+   var duration = 6000;
+   var timing = (new Date()%duration)/duration;
+
+   fill(81,158,81);
+   noStroke()
+  image(img, 250 + Math.cos(timing*2*PI)*250,          
+        250 + Math.sin(timing*2*PI)*200,       
+        60,                                     
+        60);                                   
+   
+
+    var duration = 6000;
+   var timing = (new Date()%duration)/duration;
+
+   fill(209,192,70);
+   noStroke()
+   image(img2,250 + Math.cos(timing*2*PI)*250+Math.cos(timing*4*PI)*80,          
+        250 + Math.sin(timing*2*PI)*200+Math.sin(timing*4*PI)*80,       
+        20,                                    
+        20);   
+
+        var duration = 2000;
+   var timing = (new Date()%duration)/duration;
+
+   fill(209,192,70);
+   noStroke()
+   image(img2,250 + Math.cos(timing*2*PI)*250+Math.cos(timing*4*PI)*80,          
+        250 + Math.sin(timing*2*PI)*200+Math.sin(timing*4*PI)*80,       
+        20,                                    
+        20);                                   
+}
